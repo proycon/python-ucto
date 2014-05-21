dd4b63a076c0e17abe978de57e1e43b9f86d8b2d
Maarten van Gompel <proycon@anaproy.nl>
2014-05-21 19:33:30 +0200

    added

---
+++ ucto_classes.pxd
@@ -0,0 +1,41 @@
+#*****************************
+# Python-ucto
+#   by Maarten van Gompel
+#   Centre for Language Studies
+#   Radboud University Nijmegen
+#
+#   Licensed under GPLv3
+#****************************/
+
+from libcpp.string cimport string
+from libcpp.vector cimport vector
+from libcpp cimport bool
+from libc.stdint cimport *
+
+
+
+cdef extern from "ucto/tokenize.h" namespace "Tokenizer":
+    cdef cppclass Token:
+        string texttostring()
+        string typetostring()
+        int role
+
+    cdef cppclass TokenizerClass:
+        bool init(string & settingsfile)
+
+        bool setLowercase(bool)
+        bool setUppercase(bool)
+        bool setSentenceDetection(bool)
+        bool setParagraphDetection(bool)
+        bool setQuoteDetection(bool)
+        bool setSentencePerLineOutput(bool)
+        bool setSentencePerLineInput(bool)
+        bool setXMLOutput(bool, string & docid)
+        bool setXMLInput(bool)
+        int setDebug(int)
+
+        int tokenizeLine(string &) nogil
+        vector[string] getSentences() nogil
+        vector[Token] getSentence(int) nogil
+        int countSentences(bool forcebuffer) nogil
+        int flushSentences(int count) nogil
---
+++ ucto_wrapper.pyx
@@ -0,0 +1,136 @@
+#embedsignature=True
+#*****************************
+# Python-ucto
+#   by Maarten van Gompel
+#   Centre for Language Studies
+#   Radboud University Nijmegen
+#
+#   Licensed under GPLv3
+#****************************/
+
+from libcpp.string cimport string
+from libcpp cimport bool
+from libcpp.vector cimport vector
+from cython.operator cimport dereference as deref, preincrement as inc
+from cython import address
+from libc.stdint cimport *
+from libcpp.utility cimport pair
+import os.path
+cimport ucto_classes
+
+class TokenRole:
+    NOROLE                      = 0
+    NOSPACE                     = 1
+    BEGINOFSENTENCE             = 2
+    ENDOFSENTENCE               = 4
+    NEWPARAGRAPH                = 8
+    BEGINQUOTE                  = 16
+    ENDQUOTE                    = 32
+    TEMPENDOFSENTENCE           = 64
+
+class Token:
+    def __init__(self, text, tokentype, role):
+        self.text = text
+        self.tokentype = tokentype
+        self.role = role
+
+    def __str__(self):
+        return self.text
+
+    def type(self):
+        return self.tokentype
+
+    def isendofsentence(self):
+        return self.role & TokenRole.ENDOFSENTENCE
+
+    def iseos(self): #for lazy people
+        return self.role & TokenRole.ENDOFSENTENCE
+
+    def isbeginofsentence(self):
+        return self.role & TokenRole.BEGINOFSENTENCE
+
+    def isnewparagraph(self):
+        return self.role & TokenRole.NEWPARAGRAPH
+
+    def isbeginofquote(self):
+        return self.role & TokenRole.BEGINOFQUOTE
+
+    def isendofquote(self):
+        return self.role & TokenRole.ENDOFQUOTE
+
+    def nospace(self):
+        return self.role & TokenRole.NOSPACE
+
+
+cdef class Tokenizer:
+    cdef ucto_classes.TokenizerClass tok
+
+    def __init__(self, filename, **kwargs):
+        if not os.path.exists(filename):
+            raise IOError("File does not exist, expected valid ucto settings file")
+
+        self.tok.init(filename.encode('utf-8'))
+        for arg, value in kwargs.items():
+            if arg == 'lowercase':
+                self.tok.setLowercase(value is True)
+            elif arg == 'uppercase':
+                self.tok.setUppercase(value is True)
+            elif arg == 'sentencedetection':
+                self.tok.setSentenceDetection(value is True)
+            elif arg == 'paragraphdetection':
+                self.tok.setParagraphDetection(value is True)
+            elif arg == 'quotedetection':
+                self.tok.setQuoteDetection(value is True)
+            elif arg == 'sentenceperlineinput':
+                self.tok.setSentencePerLineInput(value is True)
+            elif arg == 'sentenceperlineoutput':
+                self.tok.setSentencePerLineOutput(value is True)
+            elif arg == 'debug':
+                self.tok.setDebug(int(value))
+            else:
+                raise ValueError("No such keyword argument: " +  arg)
+
+    def process(self, str line):
+        """Feed text to the tokeniser. This needs not be a single line."""
+        return self.tok.tokenizeLine(line.encode('utf-8'))
+
+    def sentences(self):
+        cdef vector[string] results = self.tok.getSentences()
+        cdef vector[string].iterator it = results.begin()
+        cdef int sentencecount = len(results)
+        self.tok.flushSentences(sentencecount)
+        while it != results.end():
+            yield str(deref(it), 'utf-8').replace("<utt>",'')
+            inc(it)
+
+    def __iter__(self):
+        cdef int c = self.tok.countSentences(True) #forces buffer
+        cdef vector[ucto_classes.Token] v
+        cdef vector[ucto_classes.Token].iterator it
+        for i in range(0,c):
+            v = self.tok.getSentence(i)
+            it = v.begin()
+            while it != v.end():
+                tokentext = str(deref(it).texttostring(), 'utf-8')
+                tokentype = str(deref(it).typetostring(), 'utf-8')
+                role = deref(it).role
+                yield Token(tokentext, tokentype, role)
+                inc(it)
+        self.tok.flushSentences(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
