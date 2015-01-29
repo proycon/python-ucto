c1a4d6c130f5a0cec7f2dd1fbc16b8ea10963eac
Maarten van Gompel <proycon@anaproy.nl>
2015-01-29 14:50:26 +0100

    (0.2.1) expanded setup details

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 from os.path import expanduser
 HOMEDIR = expanduser("~")
-VERSION = '0.2'
+VERSION = '0.2.1'
 
 if sys.version < '3':
     extensions = [ Extension("ucto",
@@ -18,25 +18,6 @@ if sys.version < '3':
                     libraries=['ucto','folia'],
                     pyrex_gdb=True
                     ) ]
-
-    setup(
-        name = 'python-ucto',
-        version = VERSION,
-        ext_modules = extensions,
-        cmdclass = {'build_ext': build_ext},
-        requires=['ucto (>=0.8.0)'],
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Topic :: Text Processing :: Linguistic",
-            "Programming Language :: Cython",
-            "Programming Language :: Python :: 2.6",
-            "Programming Language :: Python :: 2.7",
-            "Operating System :: POSIX",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        ],
-    )
 else:
     extensions = [ Extension("ucto",
                     [ "ucto_classes.pxd", "ucto_wrapper.pyx"],
@@ -47,20 +28,29 @@ else:
                     pyrex_gdb=True
                     ) ]
 
-    setup(
-        name = 'python-ucto',
-        version = VERSION,
-        ext_modules = extensions,
-        cmdclass = {'build_ext': build_ext},
-        requires=['ucto (>=0.8.0)'],
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Topic :: Text Processing :: Linguistic",
-            "Programming Language :: Cython",
-            "Programming Language :: Python :: 3",
-            "Operating System :: POSIX",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        ],
-    )
+
+setup(
+    name = 'python-ucto',
+    version = VERSION,
+    author = 'Maarten van Gompel',
+    author_email = "proycon@anaproy.nl",
+    description = ("This is a Python binding to the tokenizer Ucto. Tokenisation is one of the first step in almost any Natural Language Processing task, yet it is not always as trivial a task as it appears to be. This binding makes the power of the ucto tokeniser available to Python. Ucto itself is a regular-expression based, extensible, and advanced tokeniser written in C++ (http://ilk.uvt.nl/ucto)."),
+    license = "GPL",
+    keywords = "tokenizer tokenization tokeniser tokenisation nlp computational_linguistics ucto",
+    url = "http://github.com/proycon/python-ucto",
+    ext_modules = extensions,
+    cmdclass = {'build_ext': build_ext},
+    requires=['ucto (>=0.8.0)'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
