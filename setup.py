25cd04f2bcba5c7296fa5fc9dffa74b381ba2c3a
Maarten van Gompel <proycon@anaproy.nl>
2023-01-03 13:12:52 +0100

    dependency update: require ucto >= 0.26

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import platform
 import os
 import sys
 
-VERSION = '0.6.1' #ensure UCTODATAVERSION in ucto_wrapper.pyx is accurate
+VERSION = '0.6.2' #ensure UCTODATAVERSION in ucto_wrapper.pyx is accurate
 
 
 includedirs = []
@@ -70,7 +70,7 @@ setup(
     url = "https://github.com/proycon/python-ucto",
     ext_modules = extensions,
     cmdclass = {'build_ext': build_ext},
-    requires=['ucto (>=0.25)'],
+    requires=['ucto (>=0.26)'],
     install_requires=['Cython'],
     data_files = [("sources",["ucto_wrapper.pyx"])],
     classifiers=[
